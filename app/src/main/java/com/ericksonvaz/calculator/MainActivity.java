49c92946ca69bcb3304423b3922afca367e55bd9
EricksonVaz <ericksoncv1@outlook.com>
2021-10-20 15:16:16 -0100

    calculator finish

--- app/src/main/java/com/ericksonvaz/calculator/MainActivity.java
+++ app/src/main/java/com/ericksonvaz/calculator/MainActivity.java
@@ -6,26 +6,51 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
 
     private TextView txtResult;
     private String stringToEvalute = "";
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearLayout = findViewById(R.id.linerWithButtons);
+
+        View rootView = this.getWindow().getDecorView().getRootView();
+
+        ArrayList<View> touchables = rootView.getTouchables();
         txtResult = findViewById(R.id.textResult);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.onBtnClick((Button) v);
+            }
+        };
+
+        for (View v: touchables) {
+            // TODO: omit button if that common listener isn't meant for it
+            if(v instanceof Button) {
+                v.setOnClickListener(listener);
+            }
+        }
     }
 
-    @Override
-    public void onClick(View v) {
-        Button btn = (Button)v;
-        switch (v.getId()){
+    protected final void onBtnClick(Button btn){
+        String btnStringId = btn.getResources().getResourceEntryName(btn.getId());
+
+        Log.d("ValueEnter", String.valueOf(btn.getText()));
+
+        switch (btn.getId()){
             case R.id.btn0:
             case R.id.btn1:
             case R.id.btn2:
@@ -36,32 +61,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn7:
             case R.id.btn8:
             case R.id.btn9:
-                stringToEvalute +=(btn.getText()+" ");
+                if(stringToEvalute.equals("0")||stringToEvalute.equals("0.0")){
+                    stringToEvalute="";
+                }
+                stringToEvalute +=(btn.getText());
                 txtResult.setText(stringToEvalute);
                 break;
             case R.id.btnClear:
-                stringToEvalute = "";
+                stringToEvalute = "0";
                 txtResult.setText("0");
+                break;
             case R.id.btnPlus:
             case R.id.btnMin:
             case R.id.btnMult:
             case R.id.btnDiv:
                 if(
+                        stringToEvalute.equals("0")||
+                                stringToEvalute.equals("")||
+                                stringToEvalute.equals(" ")){
+                    stringToEvalute="0";
+                    txtResult.setText(stringToEvalute);
+                }
+                if(
                         !stringToEvalute.endsWith("+")||
-                        !stringToEvalute.endsWith("-")||
-                        !stringToEvalute.endsWith("/")||
-                        !stringToEvalute.endsWith("*")
+                                !stringToEvalute.endsWith("-")||
+                                !stringToEvalute.endsWith("/")||
+                                !stringToEvalute.endsWith("*")
                 ){
-                    stringToEvalute +=(btn.getText()+" ");
+                    if(btn.getText().equals("x")){
+                        stringToEvalute +=(" * ");
+                    }else {
+                        stringToEvalute +=(" "+btn.getText()+" ");
+                    }
+
                     txtResult.setText(stringToEvalute);
                 }
                 break;
             case R.id.btnEq:
-                txtResult.setText(String.valueOf(eval(stringToEvalute)));
+                if(
+                        stringToEvalute.equals("0")||
+                        stringToEvalute.equals("")||
+                        stringToEvalute.equals(" ")){
+                    stringToEvalute="0";
+                }else{
+                    stringToEvalute = stringToEvalute.trim();
+                    if(
+                            stringToEvalute.endsWith("+")||
+                            stringToEvalute.endsWith("-")||
+                            stringToEvalute.endsWith("/")||
+                            stringToEvalute.endsWith("*")
+                    ){
+                        stringToEvalute += "0";
+                    }else{
+
+                        Log.d("String to evalute", stringToEvalute);
+                        txtResult.setText(String.valueOf(eval(stringToEvalute)));
+                        stringToEvalute = String.valueOf(txtResult.getText());
+                    }
+
+                }
+
                 break;
         }
-
-        Log.d("ValueEnter",String.valueOf(btn.getId()));
     }
 
     public static double eval(final String str) {
